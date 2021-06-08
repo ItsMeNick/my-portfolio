89c7e154e2ac8df1791844db9e26f9a98961be65
Nick!! <nickm4941@gmail.com>
2021-06-08 18:44:54 +0530

    deploy

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -8,7 +8,7 @@ import Education from './Education';
 
 const Pages= ({user}) =>{
     return (
-        <Router basename="/test_repository">
+        <Router>
             <Switch>
                 <Route exact path ='/'>
                     <Profile user={user} />
